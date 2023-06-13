a850f177d533335a73236290bd0b3f443a575b34
krgarav <krgarav@gmail.com>
2023-06-13 20:30:01 +0530

    Routes request added

--- firstApp.js
+++ firstApp.js
@@ -1,7 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req,res)=>{
-    console.log("Kumar Gaurav")
+   if(req.url==="/home"){
+    res.write("<h1>Welcome to home</h1>");
+    res.end();
+   }
+   if(req.url==="/about"){
+    res.write("<h1>Welcome to about us page</h1>");
+    res.end();
+   }
+   if(req.url==="/node"){
+    res.write("<h1>Welcome to my Node js Project</h1>");
+    res.end();
+   }
 })
 
-server.listen(4000)
+server.listen(4000,()=>{console.log("Server is running on port 4000")});
