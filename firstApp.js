84c98a27a09a5b5a7714fb298392f6c13225623f
krgarav <krgarav@gmail.com>
2023-06-14 17:39:25 +0530

    Basic route project

--- firstApp.js
+++ firstApp.js
@@ -1,18 +1,34 @@
 const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    const message = fs.readFileSync("./message.txt", "utf-8");
+    console.log(message);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write(`<h1>${message ? message : "Welcome to home"}</h1>`);
+    res.write(
+      "<form action='/message' method='POST'><label>Name:</label><input type='text' name='message' /> <button type='submit'>Send </button></form>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+});
 
-const server = http.createServer((req,res)=>{
-   if(req.url==="/home"){
-    res.write("<h1>Welcome to home</h1>");
-    res.end();
-   }
-   if(req.url==="/about"){
-    res.write("<h1>Welcome to about us page</h1>");
-    res.end();
-   }
-   if(req.url==="/node"){
-    res.write("<h1>Welcome to my Node js Project</h1>");
-    res.end();
-   }
-})
-
-server.listen(4000,()=>{console.log("Server is running on port 4000")});
+server.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
