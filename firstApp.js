5a3e5f13ebe66427bc59cd502dc5f7ebc88dddba
krgarav <krgarav@gmail.com>
2023-06-15 14:24:32 +0530

    express with middleware added

--- firstApp.js
+++ firstApp.js
@@ -1,8 +1,17 @@
 const http = require("http");
-const fs = require("fs");
-const request = require("./reuests");
+const express = require("express");
 
-const server = http.createServer(request);
+const app = express();
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  res.send("<h1>hello</h1>")
+next();
+});
+app.use((req, res, next) => {
+  console.log("In the next middleware");
+  
+});
+const server = http.createServer(app);
 
 server.listen(4000, () => {
   console.log("Server is runing on port 4000");
